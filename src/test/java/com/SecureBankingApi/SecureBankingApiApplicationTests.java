40dd9e2b640e3638943e6f61de0d85dc1396979a
Fuzikawa <murilopyskfuzikawa@gmail.com>
2026-01-13 21:36:16 -0300

    Build inicial do projeto de uma API bancária focada em segurança testes

---
+++ src/test/java/com/SecureBankingApi/SecureBankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SecureBankingApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureBankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
