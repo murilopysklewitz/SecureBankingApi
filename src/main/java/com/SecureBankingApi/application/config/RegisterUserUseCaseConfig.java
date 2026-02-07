a68af4b7b26f67bbef8ffcccec842411ba2db82e
Fuzikawa <murilopyskfuzikawa@gmail.com>
2026-02-06 22:18:40 -0300

    (fix)correção no arquivo de configuração para registrar um usuario

--- src/main/java/com/SecureBankingApi/application/config/RegisterUserUseCaseConfig.java
+++ src/main/java/com/SecureBankingApi/application/config/RegisterUserUseCaseConfig.java
@@ -1,5 +1,6 @@
 package com.SecureBankingApi.application.config;
 
+import com.SecureBankingApi.application.usecases.createAccount.CreateAccountUseCase;
 import com.SecureBankingApi.application.usecases.registerUser.RegisterUserUseCase;
 import com.SecureBankingApi.domain.user.ports.PasswordHasher;
 import com.SecureBankingApi.domain.user.ports.UserRepository;
@@ -11,8 +12,9 @@ public class RegisterUserUseCaseConfig {
     @Bean
     public RegisterUserUseCase registerUserUseCase(
             UserRepository repository,
-            PasswordHasher passwordHasher
+            PasswordHasher passwordHasher,
+            CreateAccountUseCase createAccountUseCase
     ){
-        return new RegisterUserUseCase(passwordHasher, repository);
+        return new RegisterUserUseCase(passwordHasher, repository, createAccountUseCase);
     }
 }
