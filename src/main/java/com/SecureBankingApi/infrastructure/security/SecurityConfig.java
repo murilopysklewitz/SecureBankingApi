fab71d22c53452bb0dd81de507cb3e7bb50b2848
Fuzikawa <murilopyskfuzikawa@gmail.com>
2026-04-30 12:38:00 -0300

    (feat) security config concluido

--- src/main/java/com/SecureBankingApi/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/SecureBankingApi/infrastructure/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -32,7 +33,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
         http
-                .csrf(csfc -> csfc.disable())
+                .csrf(AbstractHttpConfigurer::disable)
 
                 .cors(cors -> cors.configure(http))
 
